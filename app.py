9938680f4c4876e8f6cea5eff99859718b9eff59
gust204 <gugabarros40@gmail.com>
2025-08-21 21:46:47 -0300

    Atualização

--- app.py
+++ app.py
@@ -6,9 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template("inicial.html")
 
-@app.route("/carros")
-def carros():
-    return render_template("carros.html")
+@app.route("/masc")
+def masc():
+    return render_template("masc.html")
+
+@app.route("/fem")
+def fem():
+    return render_template("fem.html")
+
+@app.route("/lanc")
+def lanc():
+    return render_template("lanc.html")
+
+@app.route("/prom")
+def prom():
+    return render_template("prom.html")
 
 
 if __name__ == "__main__":
